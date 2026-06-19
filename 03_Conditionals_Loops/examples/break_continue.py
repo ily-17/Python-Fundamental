c3ec050d3552df5541d1321cf71c89e33d2c59e9
ily-17 <shirleydeborah1710@gmail.com>
2026-06-19 13:18:50 +0530

    Create break_continue.py

---
+++ 03_Conditionals_Loops/examples/break_continue.py
@@ -0,0 +1,13 @@
+print("Break Example")
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+print("\nContinue Example")
+
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
