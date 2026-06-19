995a43e0628261a119166adc3346176d6543af3c
ily-17 <shirleydeborah1710@gmail.com>
2026-06-19 13:14:27 +0530

    Create variables.py

---
+++ 02_Variables_DataTypes_Operators/examples/variables.py
@@ -0,0 +1,24 @@
+# Creating variables
+
+name = "Shirley"
+age = 25
+salary = 75000.50
+is_employee = True
+
+print("Name:", name)
+print("Age:", age)
+print("Salary:", salary)
+print("Employee:", is_employee)
+
+# Multiple assignment
+
+x, y, z = 10, 20, 30
+
+print("\nMultiple Assignment:")
+print(x, y, z)
+
+# Reassigning variables
+
+name = "John"
+
+print("\nUpdated Name:", name)
