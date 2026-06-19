be28c291b68b67e4ab31fb42d5fd9768a33c897f
ily-17 <shirleydeborah1710@gmail.com>
2026-06-19 13:15:15 +0530

    Create data_types.py

---
+++ 02_Variables_DataTypes_Operators/examples/data_types.py
@@ -0,0 +1,62 @@
+# Integer
+
+age = 25
+
+# Float
+
+salary = 75000.50
+
+# String
+
+name = "Shirley"
+
+# Boolean
+
+is_active = True
+
+# List
+
+skills = ["Python", "Java", "SQL"]
+
+# Tuple
+
+coordinates = (10, 20)
+
+# Set
+
+numbers = {1, 2, 3, 4}
+
+# Dictionary
+
+employee = {
+    "name": "Shirley",
+    "role": "AI Engineer"
+}
+
+print("Integer:", age)
+print("Float:", salary)
+print("String:", name)
+print("Boolean:", is_active)
+print("List:", skills)
+print("Tuple:", coordinates)
+print("Set:", numbers)
+print("Dictionary:", employee)
+
+print("\nChecking Data Types")
+
+print(type(age))
+print(type(salary))
+print(type(name))
+print(type(is_active))
+print(type(skills))
+print(type(coordinates))
+print(type(numbers))
+print(type(employee))
+
+print("\nType Conversion")
+
+num_str = "100"
+
+print(int(num_str))
+print(float(num_str))
+print(str(age))
