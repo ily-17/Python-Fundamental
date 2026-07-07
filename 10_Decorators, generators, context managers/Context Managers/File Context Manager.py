80a893c25ff6585f7ef82337dbdbd02f51136d84
ily-17 <shirleydeborah1710@gmail.com>
2026-07-07 13:11:28 +0530

    Create File Context Manager.py

---
+++ 10_Decorators, generators, context managers/Context Managers/File Context Manager.py
@@ -0,0 +1,17 @@
+class FileManager:
+
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        print("File Closed")
+
+
+with FileManager("test.txt", "w") as f:
+    f.write("Hello World")
